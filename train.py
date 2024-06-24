17286534e708eacf09776bdb5c9779999febe4f0
Daniel Situnayake <dan@edgeimpulse.com>
2024-06-24 15:38:14 -0400

    Apply softmax to last layer

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ class Net(nn.Module):
     def forward(self,x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.softmax(self.fc3(x), dim=1)
         return x
 
 # initialize the NN
